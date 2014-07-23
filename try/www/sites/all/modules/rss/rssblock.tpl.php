f617299fb918b01f99d1a013a6658e8825220578
Roma48 <myzfann@mail.ru>
2014-07-23 20:31:51 +0300

    m'

--- try/www/sites/all/modules/rss/rssblock.tpl.php
+++ try/www/sites/all/modules/rss/rssblock.tpl.php
@@ -9,6 +9,6 @@
 echo '<h3>Latest 10 news:</h3>';
 foreach ($item as $items) {
     echo "<h4><a href='".$items['link']."'>".$items['title']."</a></h4>";
-    echo "<p>".$items['description']."</p>";
+
 }
 ?>
